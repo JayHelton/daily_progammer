14537249509f57c35efdfeec2cb5a4a538f583f1
jaydhelton <jaydhelton@gmail.com>
2020-06-11 21:34:27 -0400

    done

--- 383/dp_383/src/main.rs
+++ 383/dp_383/src/main.rs
@@ -1,36 +1,50 @@
 use crate::problem_solving::{repeats, same_necklace};
+use std::borrow::Borrow;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::{Duration, Instant};
 
+/// Problem 3 Solution
 fn main() {
-    // This works but is very very slow
+    let start = Instant::now();
+    let size_to_find = 5;
+
     let file = File::open("./enable1.txt").expect("couldn't open file");
     let lines: Vec<String> = BufReader::new(file)
         .lines()
         .map(|x| x.expect("couldnt parse line"))
+        .filter(|x| x.len() == size_to_find)
         .collect();
 
     let new: Vec<String> = vec![];
-    let result = lines.iter().fold(new, |acc, x| {
-        // Find all the matches with the necklace challenge
-        let matches: Vec<String> = lines
-            .clone()
-            .into_iter()
-            .filter(|y| same_necklace(x.to_string(), y.to_string()))
-            .collect();
-        // I think what needs to happen is to only look forward in the list versus full iteration?
-        return if acc.len() < matches.len() {
-            println!("{} - {:?}", x, matches);
-            matches
-        } else {
-            acc
-        };
-    });
+    let result = lines
+        .iter()
+        .fold(new, |acc, x| {
+            // i dont like full iteration here. I think i should be able to remove items that showed up in results
+            // which means every iteration would have less items to compare against, but would require a lot of data shift
+            let matches: Vec<String> = lines
+                .clone()
+                .into_iter()
+                // this first filter actually makes it move a lot faster
+                .filter(|y| x.clone().chars().all(|c| y.contains(c)))
+                .filter(|y| same_necklace(x.to_string(), y.to_string()))
+                .collect();
 
-    println!("{:?}", result)
+            return if acc.len() < matches.len() {
+                println!("{} - {:?}", x, matches);
+                matches
+            } else {
+                println!("{}", x);
+                acc
+            };
+        });
+    let duration = start.elapsed();
+
+    println!("{:?} - {}", result, duration.as_secs())
 }
 
 mod problem_solving {
+    /// Problem 2 Solution
     pub fn repeats(a: String) -> usize {
         let mut a_char_vec: Vec<char> = a.chars().collect();
         let mut count = 0;
@@ -53,7 +67,12 @@ mod problem_solving {
         return counter;
     }
 
+    /// Problem 1 Solution
     pub fn same_necklace(a: String, b: String) -> bool {
+        /// This method is overkill. You can just concat the string together twice and just do a .contains
+        if a.len() != b.len() {
+            return false;
+        }
         let mut a_char_vec: Vec<char> = a.chars().collect();
         let mut count = 0;
 
