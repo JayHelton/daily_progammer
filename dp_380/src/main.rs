eb2cbd8233f0bb7d733ebd49765e2b6248ad72a2
jaydhelton <jaydhelton@gmail.com>
2020-06-17 07:24:06 -0400

    gettting close

--- dp_380/src/main.rs
+++ dp_380/src/main.rs
@@ -1,11 +1,39 @@
-use crate::smoosh_morse::smorse;
+use crate::smorse_morse::smorse;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::borrow::Borrow;
+use std::time::{Duration, Instant};
+use std::collections::HashMap;
 
 fn main() {
-    smorse("apple");
+    let start = Instant::now();
+    let mut morse_vec: HashMap<String, vec<String>> = HashMap::new();
+
+    let file = File::open("../enable1.txt").expect("couldn't open file");
+    let smorse_lines: Vec<String> = BufReader::new(file)
+        .lines()
+        .fold(morse_vec, |mut accum, curr| {
+            let morse_word: &str = curr.expect("").as_str();
+            let smorse_word: String = smorse(morse_word);
+            let map_thing: Option<Vec<String>> = accum.get(smorse_word.borrow());
+            match map_thing {
+                Some(val) => {
+                    val.push(smorse_word)
+                }
+                None => {
+                    accum.insert(smorse_word, vec! {morse_word})
+                }
+            }
+            accum
+        })
+        .collect();
+    let duration = start.elapsed();
+
+    println!("{:?}", smorse_lines);
+    println!("{}", duration.as_secs());
 }
 
-mod smoosh_morse {
-
+mod smorse_morse {
     pub fn smorse(word: &str) -> String {
         let morse_vec: Vec<&str> = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..".split(" ").collect();
         let offset: char = 'a';
@@ -14,7 +42,6 @@ mod smoosh_morse {
                 let index = ((x as u32) - (offset as u32)) as usize;
                 *morse_vec.get(index).expect("Error getting morse value")
             })
-            .into_iter()
             .collect()
     }
 
